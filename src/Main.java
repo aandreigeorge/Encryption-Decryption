1b93615d382e95591c64a46f6691f3338a5dc977
Andrei Avram <andreiavram91@gmail.com>
2024-03-29 09:31:45 +0200

    Stage 3 implementation

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,45 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        String ciphertext = scanner.nextLine();
+        String command = scanner.nextLine();
+        String message = scanner.nextLine();
         int key = scanner.nextInt();
-        String encryptedMessage = encryptMessage(ciphertext, key);
+        chooseAction(command, message, key);
+    }
+
+    static void chooseAction(String command, String text, int key){
+
+        if(command.equalsIgnoreCase("ENC")){
+            encryptMessage(text, key);
+        } else if (command.equalsIgnoreCase("DEC")){
+            decryptMessage(text, key);
+        }
+    }
+
+    static void encryptMessage(String message, int key) {
+
+        StringBuilder encryptedMessage = new StringBuilder();
+
+        for (int i = 0; i < message.length(); i++) {
+            int encryptedAscii = (message.charAt(i) + key) % 128;
+            char encryptedChar = (char) (encryptedAscii);
+            encryptedMessage.append(encryptedChar);
+        }
         System.out.println(encryptedMessage);
     }
 
-    static String encryptMessage(String message, int key) {
-        StringBuilder ciphertext = new StringBuilder();
+    static void decryptMessage(String message, int key) {
 
-        for (int i = 0; i < message.length(); i++) {
-            char currentChar = message.charAt(i);
+        StringBuilder decryptedMessage = new StringBuilder();
 
-            if (Character.isLetter(currentChar)) {
-                char encryptedChar = encryptChar(currentChar, key);
-                ciphertext.append(encryptedChar);
-            } else {
-                ciphertext.append(currentChar);
-            }
+        for(int i = 0; i< message.length(); i++) {
+            int decryptedAscii = (message.charAt(i) - key + 128) % 128;
+            char decryptedChar = (char) (decryptedAscii);
+            decryptedMessage.append(decryptedChar);
         }
-        return ciphertext.toString();
-    }
-
-    static char encryptChar(char c, int shift) {
-        return (char) ('a' + (c - 'a' + shift) % 26);
+        System.out.println(decryptedMessage);
     }
 
 }
