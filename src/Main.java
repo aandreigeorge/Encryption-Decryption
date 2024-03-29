f864203aa0eec1209593086b0b7a0b14d9994cba
Andrei Avram <andreiavram91@gmail.com>
2024-03-29 11:35:47 +0200

    Stage 4 implementation

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,49 @@
-
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
-        String message = scanner.nextLine();
-        int key = scanner.nextInt();
-        chooseAction(command, message, key);
+        Map<String, String> argumentsMap = new HashMap<>();
+        for (int i = 0; i < args.length - 1; i += 2) {
+            argumentsMap.put(args[i], args[i + 1]);
+        }
+
+
+        String mode = argumentsMap.getOrDefault("-mode", "enc");
+        int key = Integer.parseInt(argumentsMap.getOrDefault("-key", "0"));
+        String data = argumentsMap.getOrDefault("-data", "");
+
+        chooseAction(mode, data, key);
     }
 
-    static void chooseAction(String command, String text, int key){
-
-        if(command.equalsIgnoreCase("ENC")){
+    static void chooseAction(String mode, String text, int key) {
+        if (mode.equalsIgnoreCase("ENC")) {
             encryptMessage(text, key);
-        } else if (command.equalsIgnoreCase("DEC")){
+        } else if (mode.equalsIgnoreCase("DEC")) {
             decryptMessage(text, key);
         }
     }
 
     static void encryptMessage(String message, int key) {
-
         StringBuilder encryptedMessage = new StringBuilder();
 
         for (int i = 0; i < message.length(); i++) {
             int encryptedAscii = (message.charAt(i) + key) % 128;
-            char encryptedChar = (char) (encryptedAscii);
+            char encryptedChar = (char) encryptedAscii;
             encryptedMessage.append(encryptedChar);
         }
         System.out.println(encryptedMessage);
     }
 
     static void decryptMessage(String message, int key) {
-
         StringBuilder decryptedMessage = new StringBuilder();
 
-        for(int i = 0; i< message.length(); i++) {
+        for (int i = 0; i < message.length(); i++) {
             int decryptedAscii = (message.charAt(i) - key + 128) % 128;
-            char decryptedChar = (char) (decryptedAscii);
+            char decryptedChar = (char) decryptedAscii;
             decryptedMessage.append(decryptedChar);
         }
         System.out.println(decryptedMessage);
     }
-
 }
